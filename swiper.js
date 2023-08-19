47955b2161f00093c69ff1f4180b41f826cf7228
vux <vux.0511@gmail.com>
2023-08-19 22:09:36 +0700

    .

--- swiper.js
+++ swiper.js
@@ -2,15 +2,18 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 30,
     loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-
     breakpoints: {
         0: {
             slidesPerView: 1,
@@ -29,26 +32,3 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
 });
-
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-//     breakpoints: {
-//         640: {
-//             slidesPerView: 2,
-//             spaceBetween: 20,
-//         },
-//         768: {
-//             slidesPerView: 4,
-//             spaceBetween: 40,
-//         },
-//         1024: {
-//             slidesPerView: 5,
-//             spaceBetween: 50,
-//         },
-//     },
-// });
